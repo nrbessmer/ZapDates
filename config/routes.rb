afb9c13d5a87438a09e14e2b9da079e9c39d70da
Nicholas Bessmer <nrbessmer@gmail.com>
2013-09-15 01:33:28 -0400

    Updated with static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ZapDates::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
