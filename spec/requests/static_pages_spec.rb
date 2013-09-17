746caa36de55d5936a3a656a61c68d15551a6593
Nicholas Bessmer <nrbessmer@gmail.com>
2013-09-16 23:08:20 -0400

    Added routes and layout changes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,34 +5,34 @@ describe "Static pages" do
     describe "Home page" do
         
         it "should have the content 'ZapDates'" do
-            visit '/static_pages/home'
+            visit root_path
             expect(page).to have_content('ZapDates')
         end
         
         it "should have the base title" do
-            visit '/static_pages/home'
+            visit root_path
             expect(page).to have_title("ZapDates")
         end
         
         it "should not have a custom page title" do
-            visit '/static_pages/home'
+            visit root_path
             expect(page).not_to have_title('| Home')
         end
     end
     
     describe "Help page" do
         it "should have the content 'ZapDates'" do
-            visit '/static_pages/help'
+            visit help_path
             expect(page).to have_content('ZapDates')
         end
         
         it "should have the base title" do
-            visit '/static_pages/help'
+            visit help_path
             expect(page).to have_title("ZapDates")
         end
         
         it "should not have a custom page title" do
-            visit '/static_pages/help'
+            visit help_path
             expect(page).not_to have_title('| Help')
         end
     end
@@ -40,18 +40,31 @@ describe "Static pages" do
     describe "About page" do
         
         it "should have the content 'ZapDates'" do
-            visit '/static_pages/about'
+            visit about_path
             expect(page).to have_content('ZapDates')
         end
         
         it "should have the base title" do
-            visit '/static_pages/about'
+           visit about_path
             expect(page).to have_title("ZapDates")
         end
         
         it "should not have a custom page title" do
-            visit '/static_pages/about'
+            visit about_path
             expect(page).not_to have_title('| About')
+        end
+    end
+    
+    describe "Contact page" do
+        
+        it "should have the content 'Contact'" do
+            visit contact_path
+            expect(page).to have_content('Contact')
+        end
+        
+        it "should have the title 'Contact'" do
+            visit contact_path
+            expect(page).to have_title("ZapDates | Contact")
         end
     end
 end
